70952fd11193a3e48fbfb6e6a5fe96912c3d84fc
Koko-Dev <koko.webdev@gmail.com>
2022-03-11 12:11:22 -0500

    In SearchBar class component, add method onInputChange with console log denoting input change detected; on form input, add event handler onChange and point to this.onInputChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+	// todo: this method is called anytime there is an input change
+	onInputChange() {
+		console.log('Input change detected');
+	}
+
 	render() {
 		return (
 			<div className="ui segment"
@@ -10,7 +16,8 @@ class SearchBar extends React.Component {
 						<label htmlFor="search-bar">Image Search</label>
 						<input type="text"
 						       name="search-bar"
-						       id="search-bar"/>
+						       id="search-bar"
+									 onChange={this.onInputChange}/>
 					</div>
 					{/* /.field */}
 				</form>
