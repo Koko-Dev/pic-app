c9f92e43e4c44da5e1ad682c51a3f42a827ea3bd
Koko-Dev <koko.webdev@gmail.com>
2022-03-11 10:25:32 -0500

    Create components directory within src/ as mistakenly created file instead of folder and create within components/ App.js and App.css;  also, remove class component App from index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-	render() {
-		return <div>Happy Days</div>
-	}
-}
+
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
