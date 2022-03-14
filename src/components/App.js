36dbedee334dc56037f3a66a428dce4e5890884f
Koko-Dev <koko.webdev@gmail.com>
2022-03-14 11:32:49 -0400

    In App.js, change the App function into a class which extends React.Component; refactor appropriately;  We do this because it is from App.js that we want to call a search for SearchBar.js class component form input

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import SearchBar from "./SearchBar";
 
-const App = () => {
-	return (
-		<div className="ui container">
-			<h1>Hello from App.js</h1>
-			<SearchBar />
-		</div>
-	)
+class App extends React.Component {
+	render() {
+		return (
+			<div className="ui container">
+				<h1>Hello from App.js</h1>
+				<SearchBar />
+			</div>)
+	}
 }
 
 export default App;
