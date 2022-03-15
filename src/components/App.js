b0959d7795a7b1023ad7a0f5d2e83539fb724ca3
Koko-Dev <koko.webdev@gmail.com>
2022-03-15 16:48:41 -0400

    In App.js, beneath the call to <SearchBar> print out the number of images returned from the <form> <input> search on SearchBar.js; because the 'this' keyword inside of onSearchSubmit() points to prop onSubmit, convert the onSearchSubmit function into an instance property arrow function so that it can read this.state.images.length

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,22 @@ import SearchBar from "./SearchBar";
 class App extends React.Component {
 	state = { images: [] };
 
-	async onSearchSubmit(term) {
+onSearchSubmit = async (term) => {
 		const response = await axios.get('https://api.unsplash.com/search/photos', {
 			params: {query: term},
 			headers: {
 				Authorization: 'Client-ID AhUc6NEUinXjgam1yrbjhrSG50ZS-yPXoWct0-HgAxs',
 			}
 		})
-
-		console.log('this:  ', this);
-		// this.setState({ images: response.data.results });
-
+		this.setState({ images: response.data.results });
 	}
 
 	render() {
 		return (
 			<div className="ui container">
 				<h1>Hello from App.js</h1>
-				<SearchBar onSubmit={this.onSearchSubmit}/>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+
 			{/*	todo: print out number of images*/}
 				Found: {this.state.images.length} images
 			</div>)
