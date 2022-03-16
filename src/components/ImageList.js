a73d6a83610e3bb32e9a969f46b236a394ca1c94
Koko-Dev <koko.webdev@gmail.com>
2022-03-16 18:57:56 -0400

    Refactor ImageList.js

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const ImageList = (props) => {
-	console.log(props.images);
 	// todo: create an array of all image urls
 	const searchImages = props.images.map(image => {
 		return <img src={image.urls.small} />
