3f8ab045a9be9db367eadfb601615b3a3b915c81
Koko-Dev <koko.webdev@gmail.com>
2022-03-16 19:00:08 -0400

    Refactor ImageList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from "./SearchBar";
-import Image from './ImageList';
-import ImageList from "./ImageList";
+import ImageList from './ImageList';
 
 class App extends React.Component {
-	state = { images: [] };
+	state = {images: []};
 
-onSearchSubmit = async (term) => {
-		const response = await unsplash.get('/search/photos', {
-			params: {query: term}
-		})
-		this.setState({ images: response.data.results });
+	onSearchSubmit = async (term) => {
+		const response = await unsplash
+			.get('/search/photos', {
+				params: {query: term}
+			})
+		this.setState({images: response.data.results});
 	}
 
 	render() {
 		return (
 			<div className="ui container">
 				<h1>Hello from App.js</h1>
-				<SearchBar onSubmit={this.onSearchSubmit} />
+				<SearchBar onSubmit={this.onSearchSubmit}/>
 
-			{/*	todo: print out number of images*/}
+				{/*	todo: print out number of images*/}
 				<ImageList images={this.state.images}/>
 			</div>)
 	}
