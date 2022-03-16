59c2e1d599a277eea692ed7553c1ee31bde7b87e
Koko-Dev <koko.webdev@gmail.com>
2022-03-16 19:23:47 -0400

    In ImageList.js, add a prop to const searchImages map function return statement by adding key={image.id} to <img />; note that this id is coming from props.image (App.js onSearchSubmit's response.data.results); the list of image data contains a unique id and it is this id to whom we refer; we are going to do this in the event that we need the unique identifier

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const ImageList = (props) => {
+	console.log(props.images);
 	// todo: create an array of all image urls
 	const searchImages = props.images.map(image => {
-		return <img src={image.urls.small} />
+		// goal: Use id from props.images ==> App.js - response.data.results
+		return <img key={image.id} src={image.urls.regular} />
 	})
 
 	return (
