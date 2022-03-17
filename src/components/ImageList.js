cb9ea2c29f3a4b6c674a2a10dffa1e0f40a8b2ae
Koko-Dev <koko.webdev@gmail.com>
2022-03-16 20:02:37 -0400

    In ImageList.js, in const searchImages map() method, destructure out the image variable to extract description, id, and urls properties; we do this so that we don't have to call image in <img /> tag three times
    // ImageList.js
    
    const searchImages = props.images.map(({ description, id, urls }) => {
            // goal: Use id from props.images ==> App.js - response.data.results
            return <img alt={description} key={id} src={urls.regular} />
        })

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImageList = (props) => {
 	// todo: create an array of all image urls
-	const searchImages = props.images.map(({description, id, urls}) => {
+	const searchImages = props.images.map(({ description, id, urls }) => {
 		// goal: Use id from props.images ==> App.js - response.data.results
 		return <img alt={description} key={id} src={urls.regular} />
 	})
