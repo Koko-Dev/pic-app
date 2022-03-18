22f83a46210989a0a22ff42e9e03067e42688a50
Koko-Dev <koko.webdev@gmail.com>
2022-03-18 15:49:34 -0400

    In ImageCard.js, we need a work-around to be able to get the clientHeight of the image; we noted in the last commit that although the image data is received by the time we call componentDidMount(), the image has not yet been loaded from unsplash;  create an event listener within componentDidMount() which listens for the 'load' event and console logs this.imageRef.current.clientHeight; you will see that now the clientHeight of each image displays to the console

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -17,8 +17,9 @@ class ImageCard extends React.Component {
 		 image has not yet been downloaded, so we cannot
 		 call for clientHeight here; it will be zero at this point. */
 	componentDidMount() {
-		console.log(this.imageRef);
-		console.log(this.imageRef.current.clientHeight);
+		this.imageRef.current.addEventListener('load', event => {
+			console.log(this.imageRef.current.clientHeight);
+		})
 	}
 
 	render() {
