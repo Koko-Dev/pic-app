829f874c5849ce6c2909168d7260fa6b93e2c00b
Koko-Dev <koko.webdev@gmail.com>
2022-03-18 12:02:39 -0400

    In ImageList.js, import ImageCard; in searchImages(props) remove map argument destructuring and instead make the argument 'image'; change <img /> to <ImageCard />; remove alt and src as ImageCard provides both in its render return div>img; change key prop value to image.id and add prop image={image} to <ImageCard /> so that class component <ImageCard /> will have access to each image and its data; handling these images one at a time will help with styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './ImageList.css';
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
 	// todo: create an array of all image urls
-	const searchImages = props.images.map(({ description, id, urls }) => {
+	const searchImages = props.images.map((image) => {
 		// goal: Use id from props.images ==> App.js - response.data.results
-		return <img alt={description} key={id} src={urls.regular} />
+		return <ImageCard key={image.id} image={image} />
 	})
 
 	return (
