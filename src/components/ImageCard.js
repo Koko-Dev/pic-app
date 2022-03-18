d274f957c088b980963224ef0954ed1cd5264b80
Koko-Dev <koko.webdev@gmail.com>
2022-03-18 17:23:57 -0400

    In ImageCard.js, we set an inline style direction on the return div of gridRowEnd: span of this.state.spans; note this calculation was implented and state was set in setSpans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -39,7 +39,7 @@ class ImageCard extends React.Component {
 	render() {
 		const {description, urls} = this.props.image;
 		return (
-			<div style={{ gridRowEnd: `span ${this.state.spans}`}}>
+			<div style={{ gridRowEnd: `span ${this.state.spans}` }}>
 				<img
 					ref={this.imageRef}
 					alt={description}
